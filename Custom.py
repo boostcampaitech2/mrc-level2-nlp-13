2c55559d31ded13e5a9eba1191e0a487be9f6ffc
Yang junhyeok <surfing2003@naver.com>
2021-11-05 01:54:21 +0000

    refactor) update elastic search

--- Custom.py
+++ Custom.py
@@ -65,12 +65,12 @@ class MyRobertaForQuestionAnswering(RobertaPreTrainedModel):
     @add_start_docstrings_to_model_forward(
         ROBERTA_INPUTS_DOCSTRING.format("batch_size, sequence_length")
     )
-    @add_code_sample_docstrings(
-        tokenizer_class=_TOKENIZER_FOR_DOC,
-        checkpoint=_CHECKPOINT_FOR_DOC,
-        output_type=QuestionAnsweringModelOutput,
-        config_class=_CONFIG_FOR_DOC,
-    )
+    # @add_code_sample_docstrings(
+    #     tokenizer_class=_TOKENIZER_FOR_DOC,
+    #     checkpoint=_CHECKPOINT_FOR_DOC,
+    #     output_type=QuestionAnsweringModelOutput,
+    #     config_class=_CONFIG_FOR_DOC,
+    # )
     def forward(
         self,
         input_ids=None,
--- retrieval.py
+++ retrieval.py
@@ -276,69 +276,75 @@ class DenseRetrieval(RetrievalBasic):
 
 class ElasticSearch():
     def __init__(self):
-        print("Start Elastic Search init")
-        es_server = Popen(['../elastic/elasticsearch-7.9.2/bin/elasticsearch'],
+        self.es_server = Popen(['../elastic/elasticsearch-7.9.2/bin/elasticsearch'],
                    stdout=PIPE, stderr=STDOUT,
                    preexec_fn=lambda: os.setuid(1)
                   )
-        time.sleep(30)
-        es = Elasticsearch('localhost:9200')
-        if es.indices.exists('document'):
-            es.indices.delete(index='document')
-        es.indices.create(index = 'document',
-                body = {
-                    'settings':{
-                        'analysis':{
-                            'analyzer':{
-                                'my_analyzer':{
-                                    "type": "custom",
-                                    'tokenizer':'nori_tokenizer',
-                                    'decompound_mode':'mixed',
-                                    'stopwords':'_korean_',
-                                    'synonyms':'_korean_',
-                                    "filter": ["lowercase",
-                                                "my_shingle_f",
-                                                "nori_readingform",
-                                                "nori_number",
-                                                "cjk_bigram",
-                                                "decimal_digit",
-                                                "stemmer",
-                                                "trim"]
-                                }
-                            },
-                            'filter':{
-                                'my_shingle_f':{
-                                    "type": "shingle"
-                                }
+        time.sleep(10)
+        self.es = Elasticsearch('localhost:9200')
+    
+    def set_elasticsearch(self):
+
+        """
+        Summary:
+            Elastic Search에 활용할 기본틀을 만들고
+            local 서버로 wiki 데이터를 저장합니다
+        """
+        if self.es.indices.exists('document'):
+            self.es.indices.delete(index='document')
+        self.es.indices.create(index = 'document',
+            body = {
+                'settings':{
+                    'analysis':{
+                        'analyzer':{
+                            'my_analyzer':{
+                                "type": "custom",
+                                'tokenizer':'nori_tokenizer',
+                                'decompound_mode':'mixed',
+                                'stopwords':'_korean_',
+                                'synonyms':'_korean_',
+                                "filter": ["lowercase",
+                                            "my_shingle_f",
+                                            "nori_readingform",
+                                            "nori_number",
+                                            "cjk_bigram",
+                                            "decimal_digit",
+                                            "stemmer",
+                                            "trim"]
                             }
                         },
-                        'similarity':{
-                            'my_similarity':{
-                                'type':'BM25',
-                            #   'type':'boolean',
+                        'filter':{
+                            'my_shingle_f':{
+                                "type": "shingle"
                             }
                         }
                     },
-                    'mappings':{
-                        'properties':{
-                            'title':{
-                                'type':'text',
-                                'analyzer':'my_analyzer',
-                                'similarity':'my_similarity'
-                            },
-                            'text':{
-                                'type':'text',
-                                'analyzer':'my_analyzer',
-                                'similarity':'my_similarity'
-                            },
-                            'text_origin':{
-                                'type':'text',
-                                'analyzer':'my_analyzer',
-                                'similarity':'my_similarity'
-                            }
+                    'similarity':{
+                        'my_similarity':{
+                            'type':'BM25',
+                        }
+                    }
+                },
+                'mappings':{
+                    'properties':{
+                        'title':{
+                            'type':'text',
+                            'analyzer':'my_analyzer',
+                            'similarity':'my_similarity'
+                        },
+                        'text':{
+                            'type':'text',
+                            'analyzer':'my_analyzer',
+                            'similarity':'my_similarity'
+                        },
+                        'text_origin':{
+                            'type':'text',
+                            'analyzer':'my_analyzer',
+                            'similarity':'my_similarity'
                         }
                     }
                 }
+            }
         )
 
         df = make_elastic_data()
@@ -354,17 +360,13 @@ class ElasticSearch():
             buffer.append(article)
             rows += 1
             if rows % 3000 == 0:
-                helpers.bulk(es, buffer)
+                helpers.bulk(self.es, buffer)
                 buffer = []
                 print("Inserted {} articles".format(rows), end="\r")
                 time.sleep(1)
 
         if buffer:
-            helpers.bulk(es, buffer)
-        self.es = es
-
-    def get_es(self):
-        return self.es
+            helpers.bulk(self.es, buffer)
 
 class SparseRetrieval(RetrievalBasic):
     def __init__(
@@ -417,7 +419,6 @@ class SparseRetrieval(RetrievalBasic):
         self.embedding_form = embedding_form
 
     def get_sparse_embedding(self) -> NoReturn:
-
         """
         Summary:
             Passage Embedding을 만들고
@@ -457,46 +458,11 @@ class SparseRetrieval(RetrievalBasic):
 
         elif self.embedding_form == "ES":
             print("Start Elastic Search")
-            self.es = ElasticSearch()
+            es = ElasticSearch()
+            print("Set Elastic Search")
+            es.set_elasticsearch()
             print("Finish Elastic Search")
-
-
-    def build_faiss(self, num_clusters=64) -> NoReturn:
-
-        """
-        Summary:
-            속성으로 저장되어 있는 Passage Embedding을
-            Faiss indexer에 fitting 시켜놓습니다.
-            이렇게 저장된 indexer는 `get_relevant_doc`에서 유사도를 계산하는데 사용됩니다.
-
-        Note:
-            Faiss는 Build하는데 시간이 오래 걸리기 때문에,
-            매번 새롭게 build하는 것은 비효율적입니다.
-            그렇기 때문에 build된 index 파일을 저정하고 다음에 사용할 때 불러옵니다.
-            다만 이 index 파일은 용량이 1.4Gb+ 이기 때문에 여러 num_clusters로 시험해보고
-            제일 적절한 것을 제외하고 모두 삭제하는 것을 권장합니다.
-        """
-
-        indexer_name = f"faiss_clusters{num_clusters}.index"
-        indexer_path = os.path.join(self.data_path, indexer_name)
-        if os.path.isfile(indexer_path):
-            print("Load Saved Faiss Indexer.")
-            self.indexer = faiss.read_index(indexer_path)
-
-        else:
-            p_emb = self.p_embedding.astype(np.float32).toarray()
-            emb_dim = p_emb.shape[-1]
-
-            num_clusters = num_clusters
-            quantizer = faiss.IndexFlatL2(emb_dim)
-
-            self.indexer = faiss.IndexIVFScalarQuantizer(
-                quantizer, quantizer.d, num_clusters, faiss.METRIC_L2
-            )
-            self.indexer.train(p_emb)
-            self.indexer.add(p_emb)
-            faiss.write_index(self.indexer, indexer_path)
-            print("Faiss Indexer Saved.")
+            self.es = es
 
     def retrieve(
         self, query_or_dataset: Union[str, Dataset], topk: Optional[int] = 1
@@ -605,7 +571,6 @@ class SparseRetrieval(RetrievalBasic):
             doc_indices = sorted_result.tolist()[:k]
 
             return doc_score, doc_indices
-        
 
         elif self.embedding_form == "ES":
             res = self.es.search(index = "document",q=query, size=k)
